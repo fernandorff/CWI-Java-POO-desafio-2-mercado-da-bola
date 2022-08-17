c85d4a3aa85e1dadab877abfb42409b8b23ba8ff
fernandorff <fernandorff92@gmail.com>
2022-08-17 08:58:13 -0300

    aumentando coverage

--- src/test/java/MercadoDaBolaTest.java
+++ src/test/java/MercadoDaBolaTest.java
@@ -1,9 +1,9 @@
 import clube.Clube;
-import jogador.Goleiro;
-import jogador.Zagueiro;
-import jogador.Atacante;
-import jogador.MeioCampo;
 import clube.Negociacao;
+import jogador.Atacante;
+import jogador.Goleiro;
+import jogador.MeioCampo;
+import jogador.Zagueiro;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -13,64 +13,63 @@ public class MercadoDaBolaTest {
     public void deveSerPossivelNegociarUmGoleiroComUmClubeQueTemSaldoEmCaixa() {
 
         Negociacao negociacao = new Negociacao();
+
         Clube clube = new Clube("Grêmio", 10, 1680);
-        Goleiro goleiro = new Goleiro("Marcelo Grohe", 33, null, 8, 1,1000 , 5);
+
+        Goleiro goleiro = new Goleiro("Marcelo Grohe", 33, null, 8, 1, 1000, 5);
 
         boolean foiPossivelNegociar = negociacao.negociar(clube, goleiro);
 
-        Assert.assertEquals(foiPossivelNegociar, true);
+        Assert.assertEquals(true, foiPossivelNegociar);
+
     }
 
     @Test
     public void naoDeveSerPossivelNegociarUmAtacanteComUmClubeQueTemReputacaoHistoricaMenorQueASua() {
 
         Negociacao negociacao = new Negociacao();
+
         Clube clube = new Clube("Internacional", 3, 10000);
+
         Atacante atacante = new Atacante("Cristiano Ronaldo", 35, null, 9, 1, 1000, 20);
 
         boolean foiPossivelNegociar = negociacao.negociar(clube, atacante);
 
-        Assert.assertEquals(foiPossivelNegociar, false);
+        Assert.assertEquals(false, foiPossivelNegociar);
+
     }
 
     @Test
     public void naoDeveSerPossivelNegociarPorFaltaDeCaixaDisponivel() {
 
         Negociacao negociacao = new Negociacao();
+
         Clube clube = new Clube("Internacional", 3, 10);
+
         Zagueiro zagueiro = new Zagueiro("Cafu", 35, null, 7, 1, 1000);
 
         boolean foiPossivelNegociar = negociacao.negociar(clube, zagueiro);
 
-        Assert.assertEquals(foiPossivelNegociar, false);
+        Assert.assertEquals(false, foiPossivelNegociar);
+
     }
 
     @Test
     public void deveCalcularCorretamenteOPrecoDoMeioCampoComMenosDeTrintaAnos() {
 
-        MeioCampo kaka = new MeioCampo("Kaka",28,null,6,0, 1000);
+        MeioCampo kaka = new MeioCampo("Kaka", 28, null, 6, 0, 1000);
 
-        double valorDeMercadoEsperado = 1000;
-
-        Assert.assertEquals(kaka.getValorDeMercado(), valorDeMercadoEsperado,0.01);
-
+        Assert.assertEquals(1000, kaka.getValorDeMercado(), 0.01);
 
     }
 
     @Test
     public void deveCalcularCorretamenteOPrecoDoMeioCampoComMaisDeTrintaAnos() {
 
-        MeioCampo kaka = new MeioCampo("Kaka",35,null,6,0, 1000);
+        MeioCampo kaka = new MeioCampo("Kaka", 35, null, 6, 0, 1000);
 
-        double valorDeMercadoEsperado = 700;
-
-        Assert.assertEquals(kaka.getValorDeMercado(), valorDeMercadoEsperado,0.01);
-
+        Assert.assertEquals(700, kaka.getValorDeMercado(), 0.01);
 
     }
-
-
-
-
 
 }
